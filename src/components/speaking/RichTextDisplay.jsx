8bde28def3022ab9688297ed89e15d34ddeeb01f
marshallmckenzie <marshallmckenzie@adventistconnect.org>
2026-07-27 01:01:03 +0000

    File changes

--- src/components/speaking/RichTextDisplay.jsx
+++ src/components/speaking/RichTextDisplay.jsx
@@ -1,28 +1,11 @@
-const SCRIPT_TAGS = new Set(['SCRIPT', 'STYLE', 'IFRAME', 'OBJECT', 'EMBED', 'LINK', 'META', 'BASE', 'FORM']);
-
-function sanitize(html) {
-  if (!html) return '';
-  const doc = new DOMParser().parseFromString(String(html), 'text/html');
-  doc.querySelectorAll(Array.from(SCRIPT_TAGS).join(',')).forEach((n) => n.remove());
-  doc.querySelectorAll('*').forEach((el) => {
-    [...el.attributes].forEach((attr) => {
-      const name = attr.name.toLowerCase();
-      if (name.startsWith('on')) {
-        el.removeAttribute(attr.name);
-      } else if ((name === 'href' || name === 'src') && /^\s*javascript:/i.test(attr.value)) {
-        el.removeAttribute(attr.name);
-      }
-    });
-  });
-  return doc.body.innerHTML;
-}
+import DOMPurify from 'dompurify';
 
 export default function RichTextDisplay({ html }) {
   if (!html) return null;
   return (
     <div
       className="rich-notes-display prose-sm"
-      dangerouslySetInnerHTML={{ __html: sanitize(html) }}
+      dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(html) }}
     />
   );
 }
